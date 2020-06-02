db8fd172d402dc34aa548becb306a51c87694fa9
doug9507 <dpbarbaran7@gmail.com>
2020-06-01 20:45:15 -0500

    Deploy proyecto

---
+++ src/js/main.js
@@ -0,0 +1,38 @@
+const app = new Vue({
+	el: "#app",
+	data: {
+		tareas: [],
+		nuevaTarea: "",
+	},
+	methods: {
+		agregarTarea() {
+			if (this.nuevaTarea !== "") {
+				this.tareas.push({ nombre: this.nuevaTarea, estado: false });
+				// console.log({nombre:this.nuevaTarea,estado:false})
+				// console.log(this.tareas)
+				localStorage.setItem("tareas", JSON.stringify(this.tareas));
+				this.nuevaTarea = "";
+			} else {
+				swal("No seas imbecil!", "Escribe algo!", "warning");
+			}
+		},
+		tareaRealizado(index) {
+			// console.log(this.tareas[index])
+			this.tareas[index].estado = true;
+			localStorage.setItem("tareas", JSON.stringify(this.tareas));
+		},
+		tareaEliminado(index) {
+			this.tareas.splice(index, 1);
+			localStorage.setItem("tareas", JSON.stringify(this.tareas));
+		},
+	},
+	created: function () {
+		// console.log(JSON.parse(localStorage.getItem('tareas')))
+		const localDB = JSON.parse(localStorage.getItem("tareas"));
+		if (localDB) {
+			this.tareas = localDB;
+		} else {
+			this.tareas = [];
+		}
+	},
+});
